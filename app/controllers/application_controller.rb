1232bf04f7bad5412a1ba4e5898383f078db1ab5
Peyton Harrison <peyton006@hotmail.com>
2022-09-12 16:59:32 -0500

    commit tree be86df6214037ed53f27db48c2c55ccc4af5d7de

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
   # add routes
+  get '/' do
+    '<h3>Hello World<h3>'
+  end
+  
+  get '/bakeries' do
+    # get all the bakeries from the database
+    bakeries = Bakery.all
+    # send them back as a JSON array
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(inlcude: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    Baked_good.all.order(:price).reverse
+  end
 
 end
